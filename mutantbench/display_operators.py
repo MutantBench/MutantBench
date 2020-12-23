e4c20618fe253a34da96a472f8604ef7b9a0583f
Lars van Hijfte <larsvanhijfte@hotmail.com>
2020-12-23 21:42:26 +0100

    Expanded visualization to include per program latex table

--- mutantbench/display_operators.py
+++ mutantbench/display_operators.py
@@ -1,32 +1,94 @@
+from sqlalchemy import asc
 from matplotlib import pyplot as plt
 from mutantbench import db, session
 
 
-def main():
+def operators():
     bins = []
     totals = []
     equivs = []
     labels = []
-    for operator in session.query(db.Operator):
+    for operator in session.query(db.Operator).order_by(db.Operator.name):
         bins.append(operator.name)
         labels.append('$' + ' '.join(operator.description.split('\n')) + '$')
 
-        totals.append((session.query(db.Mutant).filter(
-            db.Mutant.operators.contains(operator)).count()
-        ))
+        totals.append(
+            session.query(db.Mutant)
+            .filter(db.Mutant.operators.contains(operator))
+            .distinct()
+            .count()
+        )
 
-        equivs.append((session.query(db.Mutant).filter(
-            db.Mutant.operators.contains(operator),
-            db.Mutant.equivalent
-        ).count()))
+        equivs.append(
+            session.query(db.Mutant)
+            .filter(
+                db.Mutant.operators.contains(operator),
+                db.Mutant.equivalent
+            )
+            .distinct()
+            .count()
+        )
 
-    plt.bar(bins, totals, label='total')
-    plt.bar(bins, equivs, label='equivalent')
+    plt.bar(bins, totals, label='total', orientation=u'vertical')
+    plt.bar(bins, equivs, label='equivalent', orientation=u'vertical')
+
+    ticks_and_labels = plt.xticks(range(len(bins)), bins, rotation=0)
+    for i, label in enumerate(ticks_and_labels[1]):
+        label.set_y(label.get_position()[1] - (i % 2) * 0.05)
 
     # plt.legend(prop={'family': 'monospace'})
     plt.legend()
-    plt.show()
+    plt.savefig('combined_dataset_operator_stats.pdf')
+
+
+def programs():
+    bins = []
+    mutant_count = []
+    equiv_count = []
+    program_size = []
+    language = []
+    labels = []
+    for program in session.query(db.Program).filter(db.Program.language == 'c').order_by(asc(db.Program.file_name)):
+        bins.append(program.name)
+        mutant_count.append(session.query(db.Mutant).filter(
+            db.Mutant.program == program).count()
+        )
+        equiv_count.append(session.query(db.Mutant).filter(
+            db.Mutant.program == program, db.Mutant.equivalent).count())
+        program_size.append(len(open(program.path, 'r').readlines()))
+        language.append(program.language)
+
+    print('\\hline')
+    print('\\multicolumn{4}{l}{\\textit{C}} \\\\ \\hline')
+    for name, mutants, equiv, size in zip(bins, mutant_count, equiv_count, program_size):
+        print(f'{name.replace("_", " ")} & {mutants} & {round(equiv / mutants * 100, 2)} & {size}  \\\\ ')
+    bins = []
+    mutant_count = []
+    equiv_count = []
+    program_size = []
+    language = []
+    labels = []
+    for program in session.query(db.Program).filter(db.Program.language == 'java').order_by(asc(db.Program.file_name)):
+        bins.append(program.name)
+        mutant_count.append(session.query(db.Mutant).filter(
+            db.Mutant.program == program).count()
+        )
+        equiv_count.append(session.query(db.Mutant).filter(
+            db.Mutant.program == program, db.Mutant.equivalent).count())
+        program_size.append(len(open(program.path, 'r').readlines()))
+        language.append(program.language)
+
+    print('\\hline')
+    print('\\multicolumn{4}{l}{\\textit{Java}} \\\\ \\hline')
+    for name, mutants, equiv, size in zip(bins, mutant_count, equiv_count, program_size):
+        print(f'{name.replace("_", " ")} & {mutants} & {round(equiv / mutants * 100, 2)} & {size}  \\\\ ')
+    print('\\hline')
+    print(f' Total (C \\& Java) & {session.query(db.Mutant).count()} & {round(session.query(db.Mutant).filter(db.Mutant.equivalent).count() / session.query(db.Mutant).count() * 100, 2)} &   \\\\ ')
+    print('\\hline')
+
+
 
 
 if __name__ == '__main__':
-    main()
+    operators()
+    programs()
